80e667fef7247f26b00feb409ec83e8fa551c234
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-05-27 15:13:05 +0300

    Implement capacity check function and average calorie calculation

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,22 @@
-function filterArray(numbers, value) {
-  const filtered = [];
-  for (let i = 0; i < numbers.length; i++) {
-    const number = numbers[i];
-    if (number > value) {
-      filtered.push(number);
-    }
-  }
-  return filtered;
-}
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+};
+
+profile.changeUsername = function (newName) {
+  this.username = newName;
+};
+profile.updatePlayTime = function (hours) {
+  this.playTime += hours;
+};
+profile.getInfo = function () {
+  return `${this.username} has ${this.playTime} active hours!`;
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
